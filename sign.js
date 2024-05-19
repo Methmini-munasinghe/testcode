e4abccb15f922c72d7d4e6092cda13bec6b80423
Methmini <167952026+Methmini-munasinghe@users.noreply.github.com>
2024-05-20 02:10:32 +0530

    assignment web application

---
+++ sign.js
@@ -0,0 +1,42 @@
+document.getElementById("showpassword").addEventListener("click",function(){
+    const passwordinput = document.getElementById("password");
+    const type = passwordinput.getAttribute("type")==="password"?"text":"password";
+passwordinput.setAttribute("type",type);
+let highpass = document.getElementById("hidepassword");
+
+
+
+this.classList.toggle("fa-eye-slash");
+highpass.style.display="block";
+
+});
+document.getElementById("hidepassword").addEventListener("click",function(){
+    const passwordinput = document.getElementById("password");
+    const type = passwordinput.getAttribute("type")==="password"?"text":"password";
+passwordinput.setAttribute("type",type);
+let highpass = document.getElementById("showpassword");
+
+
+this.classList.toggle("fa-eye");
+// highpass.style.display="block";
+
+});
+
+
+
+
+document.getElementById("showpassword1").addEventListener("click", function() {
+    const passwordInput = document.getElementById("repassword");
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye-slash");
+    document.getElementById("hidepassword1").style.display = "block";
+  });
+
+  document.getElementById("hidepassword1").addEventListener("click", function() {
+    const passwordInput = document.getElementById("repassword");
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye");
+    // document.getElementById("showpassword1").style.display = "none";
+  });
